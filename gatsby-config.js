1ca79b82f1fe7707c16ca8c3e1b42a9b268c3edd
Leo <tl.leolee@gmail.com>
2020-01-17 21:41:58 -0500

    initial commit, set up project

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/src/fonts/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/components/Hero/hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './hero.scss';
+
+const Hero = (props) => {
+  return (
+    <section className="hero">
+      <h1>Hero</h1>
+    </section>
+  )
+}
+
+export default Hero;
