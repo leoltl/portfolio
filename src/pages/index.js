920d935c959886d8eeeab2f760209ac26161724a
Leo <tl.leolee@gmail.com>
2020-01-19 10:57:31 -0500

    added graphql query to project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { graphql } from 'gatsby';
 
 import Hero from '../components/Hero/hero';
 import About from '../components/About/about';
@@ -8,11 +9,30 @@ import Layout from '../components/Layout/layout';
 
 import '../components/base.scss';
 
-const IndexPage = () => (
+export const query = graphql`
+  query MyQuery {
+    allProject {
+      nodes {
+        featured
+        id
+        title
+        demoUrl
+        description
+        imageUrl
+        problemStatement
+        srcUrl
+        techStack
+        team
+      }
+    }
+}
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <Hero />
     <About />
-    <Projects />
+    <Projects projects={data.allProject.nodes} />
     <Connect />
   </Layout>
 )
