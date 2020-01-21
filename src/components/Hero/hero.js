ebdafe1ed81104dff5bd29d6dedcd4002e23cf2b
leoltl <tl.leolee@gmail.com>
2020-01-20 22:59:20 -0500

    minor edit

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -10,7 +10,7 @@ const Hero = (props) => {
         <div className="hero__greeting">
           <h2>Hello,</h2>
           <h1>I am Leo Lee.</h1>
-          <h2>Front End Web Developer</h2>
+          <h2>Full Stack Web Developer</h2>
         </div>
         <div className="hero__tagline">
           <p>I'm a software developer based in Toronto, ON. Passionate about building web applications that marries good user experience design.</p>
